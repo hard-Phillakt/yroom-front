4d4f9a53b77b93f5e9a14badd51e73f10739b66a
hard-phillakt <hard-phillakt@mail.ru>
2020-01-20 17:52:06 +0300

    add new layers and 4 pages

--- dist/js/main.js
+++ dist/js/main.js
@@ -10623,18 +10623,75 @@ $(document).ready(function () {
 
 //   mobile menu
 
-    $('.hamburger').on('click', function () {
-
+    function eventMenu() {
         if(!$('.hamburger').hasClass('is-active')){
             $('.hamburger').addClass('is-active')
             $('.header-mobile-menu').addClass('view-menu');
             $('.white-layer').addClass('view-layer');
+            $('body').css({
+                overflow: 'hidden'
+            })
         }else {
             $('.hamburger').removeClass('is-active')
             $('.header-mobile-menu').removeClass('view-menu');
             $('.white-layer').removeClass('view-layer');
+            $('body').css({
+                overflow: 'inherit'
+            })
         }
+    }
 
+
+    $('.hamburger').on('click', function () {
+        eventMenu();
+    });
+
+    $('.white-layer').on('click', function (e) {
+        eventMenu();
+    });
+
+
+
+    $('.button__collapse').on('click', function () {
+        var arrow = $(this).find('.button__collapse_arrow-bottom');
+
+        if(arrow.hasClass('rotate-top')){
+            arrow.removeClass('rotate-top');
+        }else {
+            arrow.addClass('rotate-top');
+        }
     })
 
+
+//    Map
+
+
+    var map = document.querySelector('#map');
+
+    if (map) {
+        ymaps.ready(function () {
+
+            var myMap = new ymaps.Map('map', {
+                    center: [50.638953, 36.573748],
+                    zoom: 17,
+                    // controls: []
+                }, {
+                    searchControlProvider: 'yandex#search'
+                });
+
+            myMap.behaviors.disable(['scrollZoom', 'rightMouseButtonMagnifier']);
+
+            var myGeoObject = new ymaps.GeoObject({
+                geometry: {
+                    type: "Point", // тип геометрии - точка
+                    coordinates: [50.638953, 36.573748] // координаты точки
+                }
+            }, {
+                preset: 'islands#yellowIcon'
+            });
+
+            myMap.geoObjects
+                .add(myGeoObject);
+        });
+    }
 });
